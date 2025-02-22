7000bb6d248fa7e0bba6a564215d72c08fed5626
offerrall <offerrallps4@gmail.com>
2025-02-22 09:11:20 +0100

    Add dimension check for load_image function in CudaImage

--- photoff/io/__init__.py
+++ photoff/io/__init__.py
@@ -14,13 +14,19 @@ def save_image(image: CudaImage,
     img = Image.frombytes("RGBA", (image.width, image.height), bytes(img_data))
     img.save(filename)
 
-def load_image(filename: str, container: CudaImage = None) -> CudaImage:
+def load_image(filename: str,
+               container: CudaImage = None) -> CudaImage:
+
     img = Image.open(filename).convert("RGBA")
     width, height = img.size
-    img_array = np.asarray(img, dtype=np.uint8)
 
     if container is None:
         container = CudaImage(width, height)
+
+    if width > container.width or height > container.height:
+        raise ValueError("Image dimensions exceed container dimensions")
+
+    img_array = np.asarray(img, dtype=np.uint8)
 
     c_buffer = ffi.cast("uchar4*", img_array.ctypes.data)
     copy_to_device(container.buffer, c_buffer, width, height)
