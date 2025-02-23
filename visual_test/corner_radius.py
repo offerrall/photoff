9a6c22f11037e24bcec0cbd712072ea34e500f42
offerrall <offerrallps4@gmail.com>
2025-02-23 09:35:46 +0100

    Update corner_border_ui to use a default border size of 500 and adjust image dimensions to 1000x1000

--- visual_test/corner_radius.py
+++ visual_test/corner_radius.py
@@ -7,11 +7,11 @@ from typing import Annotated
 from functogui import App, imageFileReturn, intUi
 
 def corner_border_ui(color: tuple[int, int, int, int] = (255, 0, 0, 255),
-                     border_size: Annotated[int, intUi(min_value=1, max_value=500)] = 100
+                     border_size: Annotated[int, intUi(min_value=1, max_value=500)] = 500
                      ) -> Annotated[str, imageFileReturn]:
     
     path = "./visual_test/corner_border.png"
-    image = CudaImage(1920, 1080)
+    image = CudaImage(1000, 1000)
 
     fill_color(image, RGBA(*color))
     apply_corner_radius(image, border_size)
