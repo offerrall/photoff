338597f70121bdd5133e40f8b4a419500e544c2b
offerrall <offerrallps4@gmail.com>
2025-02-23 08:13:35 +0100

    Rename blend_buffers function to blend and add UI for blending images

--- photoff/operations/blend.py
+++ photoff/operations/blend.py
@@ -1,5 +1,6 @@
 from ..core import _lib
 from ..core.types import CudaImage
 
-def blend_buffers(dst: CudaImage, src: CudaImage, x: int, y: int) -> None:
+def blend(dst: CudaImage, src: CudaImage, x: int, y: int) -> None:
+    
     _lib.blend_buffers(dst.buffer, src.buffer, dst.width, dst.height, src.width, src.height, x, y)
---
+++ visual_test/blend.py
@@ -0,0 +1,36 @@
+from photoff.operations.blend import blend
+from photoff.operations.fill import fill_color
+from photoff.io import save_image
+from photoff import RGBA, CudaImage
+
+from typing import Annotated
+from functogui import App, imageFileReturn, intUi
+
+
+def blend_ui(color1: tuple[int, int, int, int] = (255, 0, 0, 255),
+             color2: tuple[int, int, int, int] = (0, 255, 0, 128),
+             color2_height: Annotated[int, intUi(min_value=1, max_value=1080)] = 300,
+             color2_width: Annotated[int, intUi(min_value=1, max_value=1920)] = 300,
+             x: Annotated[int, intUi(min_value=0, max_value=1920)] = 0,
+             y: Annotated[int, intUi(min_value=0, max_value=1080)] = 0
+            
+             ) -> Annotated[str, imageFileReturn]:
+
+    path = "./visual_test/blend.png"
+    image1 = CudaImage(1920, 1080)
+    image2 = CudaImage(color2_width, color2_height)
+
+    fill_color(image1, RGBA(*color1))
+    fill_color(image2, RGBA(*color2))
+
+    blend(image1, image2, x, y)
+    save_image(image1, path)
+    image1.free()
+    image2.free()
+
+    return path
+
+
+
+
+App(blend_ui)
