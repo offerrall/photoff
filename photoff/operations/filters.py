a7067de33f57f6ac7116038e29e1ee3996862d8a
offerrall <offerrallps4@gmail.com>
2025-02-23 10:40:10 +0100

    Add newline at the end of apply_stroke function in filters.py

--- photoff/operations/filters.py
+++ photoff/operations/filters.py
@@ -6,6 +6,7 @@ def apply_corner_radius(image: CudaImage, size: int) -> None:
     _lib.apply_corner_radius(image.buffer, image.width, image.height, size)
 
 def apply_stroke(image: CudaImage, stroke_width: int, stroke_color: RGBA) -> None:
+    
     _lib.apply_stroke(image.buffer, image.width, image.height,
                       stroke_width, stroke_color.r, stroke_color.g,
                       stroke_color.b, stroke_color.a)
