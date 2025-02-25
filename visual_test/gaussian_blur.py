44c773d1ed3f61bb587979d79038bf7ea6bba039
offerrall <offerrallps4@gmail.com>
2025-02-25 16:04:40 +0100

    Update gaussian_blur_ui to use int type for radius and ensure proper conversion to float

--- visual_test/gaussian_blur.py
+++ visual_test/gaussian_blur.py
@@ -2,16 +2,18 @@ from photoff.operations.filters import apply_gaussian_blur, apply_corner_radius
 from photoff.io import save_image, load_image
 from photoff import CudaImage
 from typing import Annotated
-from functogui import App, imageFileReturn, floatUi, fileUi
+from functogui import App, imageFileReturn, intUi, fileUi
 
 def gaussian_blur_ui(image: Annotated[str, fileUi] = "./assets/stock.jpg",
-                     radius: Annotated[float, floatUi(min_value=0, max_value=50.0)] = 5.0
+                     radius: Annotated[int, intUi(min_value=0, max_value=50)] = 5
                      ) -> Annotated[str, imageFileReturn]:
 
     path = "./assets/gaussian_blur_test.png"
     src_image = load_image(image)
     if radius == 0:
         radius = 0.1
+    
+    radius = float(radius)
 
     aux_buffer = CudaImage(src_image.width, src_image.height)
     apply_gaussian_blur(src_image, radius)
