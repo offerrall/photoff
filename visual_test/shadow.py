64785bfa6b73db40882d586f5c8a6a668d95f1ca
offerrall <offerrallps4@gmail.com>
2025-02-25 05:44:34 +0100

    Refactor shadow_ui function to enhance parameter validation and add image_copy_cache support in apply_shadow

--- visual_test/shadow.py
+++ visual_test/shadow.py
@@ -1,24 +1,24 @@
 from photoff.operations.filters import apply_shadow
 from photoff.io import save_image, load_image
-from photoff import RGBA
-from typing import Annotated
-from functogui import App, imageFileReturn, floatUi, fileUi
+from photoff import RGBA, CudaImage
 
-def shadow_ui(image: Annotated[str, fileUi] = "./assets/logo.png",
-              radius: Annotated[float, floatUi(min_value=1.0, max_value=100.0)] = 10.0,
-              intensity: Annotated[float, floatUi(min_value=0.0, max_value=1.0)] = 0.5,
-              shadow_color: tuple[int, int, int, int] = (0, 0, 0, 128),
-              inner: bool = False
-              ) -> Annotated[str, imageFileReturn]:
-    
+from typing import Annotated
+from functogui import App, imageFileReturn, fileUi, floatUi
+
+def shadow_ui(
+    image: Annotated[str, fileUi] = "./assets/logo.png",
+    radius: Annotated[float, floatUi(min_value=1.0, max_value=50.0)] = 10.0,
+    intensity: Annotated[float, floatUi(min_value=0.1, max_value=1.0)] = 0.5,
+    shadow_color: tuple[int, int, int, int] = (0, 0, 0, 128),
+    inner: bool = False
+) -> Annotated[str, imageFileReturn]:
     path = "./assets/shadow_test.png"
     src_image = load_image(image)
     
-    apply_shadow(src_image, radius, intensity, RGBA(*shadow_color), inner)
+    apply_shadow(src_image, radius, intensity, RGBA(*shadow_color), image_copy_cache=None, inner=inner)
+    
     save_image(src_image, path)
-    
     src_image.free()
-    
     return path
 
 App(shadow_ui)
