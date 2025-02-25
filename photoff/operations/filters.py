72fa58dc39c841d149a17ef6b6a0b793d8953aa5
offerrall <offerrallps4@gmail.com>
2025-02-25 05:22:36 +0100

    Implement apply_stroke function with buffer copying; update stroke_ui parameters for improved usability

--- photoff/operations/filters.py
+++ photoff/operations/filters.py
@@ -1,5 +1,6 @@
 from ..core import _lib
 from ..core.types import CudaImage, RGBA
+from ..core.buffer import copy_buffers_same_size
 
 def apply_corner_radius(image: CudaImage, size: int) -> None:
 
@@ -21,3 +22,28 @@ def apply_flip(image: CudaImage,
                     image.height,
                     flip_horizontal,
                     flip_vertical)
+
+def apply_stroke(image: CudaImage,
+                 stroke_width: int,
+                 stroke_color: RGBA,
+                 image_copy_cache: CudaImage = None,
+                 inner: bool = False) -> None:
+    
+    if image_copy_cache is None:
+        image_copy_cache = CudaImage(image.width, image.height)
+        copy_buffers_same_size(image.buffer, image_copy_cache.buffer, image.width, image.height)
+    else:
+        if image_copy_cache.width != image.width or image_copy_cache.height != image.height:
+            raise ValueError(f"Destination image dimensions must match original image dimensions: {image.width}x{image.height}, got {image_copy_cache.width}x{image_copy_cache.height}")
+
+    _lib.apply_stroke(image.buffer,
+                      image_copy_cache.buffer,
+                      image.width,
+                      image.height,
+                      stroke_width,
+                      stroke_color.r,
+                      stroke_color.g,
+                      stroke_color.b,
+                      stroke_color.a,
+                      int(inner))
+    
--- visual_test/stroke.py
+++ visual_test/stroke.py
@@ -1,24 +1,24 @@
 from photoff.operations.filters import apply_stroke
 from photoff.io import save_image, load_image
-from photoff import RGBA
+from photoff import RGBA, CudaImage
 
 from typing import Annotated
-from functogui import App, imageFileReturn, intUi, fileUi
+from functogui import App, imageFileReturn, fileUi, intUi
 
-def stroke_ui(image: Annotated[str, fileUi] = "./assets/logo.png",
-              stroke_width: Annotated[int, intUi(min_value=1, max_value=200)] = 5,
-              stroke_color: tuple[int, int, int, int] = (0, 255, 0, 255),
-              inner: bool = False
-              ) -> Annotated[str, imageFileReturn]:
-    
+def stroke_ui(
+    image: Annotated[str, fileUi] = "./assets/logo.png",
+    stroke_width: Annotated[int, intUi(min_value=1, max_value=50)] = 10,
+    stroke_color: tuple[int, int, int, int] = (255, 0, 0, 255),
+    inner: bool = True
+) -> Annotated[str, imageFileReturn]:
     path = "./assets/stroke_test.png"
     src_image = load_image(image)
+    src_image_copy = load_image(image)
     
-    apply_stroke(src_image, stroke_width, RGBA(*stroke_color), inner)    
+    apply_stroke(src_image, stroke_width, RGBA(*stroke_color), image_copy_cache=src_image_copy, inner=inner)
+    
     save_image(src_image, path)
-    
     src_image.free()
-
     return path
 
 App(stroke_ui)
