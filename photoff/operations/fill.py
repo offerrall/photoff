516f7c16dea1aa620df4f0c0c945fcc83cf7c18d
offerrall <offerrallps4@gmail.com>
2025-02-25 02:28:29 +0100

    Add whitespace for improved readability in fill.py

--- photoff/operations/fill.py
+++ photoff/operations/fill.py
@@ -2,9 +2,11 @@ from ..core import _lib
 from ..core.types import CudaImage, RGBA
 
 def fill_color(image: CudaImage, color: RGBA) -> None:
+    
     _lib.fill_color(image.buffer, image.width, image.height, color.r, color.g, color.b, color.a)
 
 def fill_gradient(image: CudaImage, color1: RGBA, color2: RGBA, direction: int, seamless: bool) -> None:
+    
     _lib.fill_gradient(
         image.buffer,
         image.width,
