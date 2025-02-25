013093f2e20551544a1faedf7cf90c5f96361fc4
offerrall <offerrallps4@gmail.com>
2025-02-25 10:02:42 +0100

    Add cover_image_in_container function to handle image resizing and blending within a specified container

--- photoff/operations/utils.py
+++ photoff/operations/utils.py
@@ -1,9 +1,10 @@
 from ..core.types import CudaImage
 from .blend import blend
-
+from .fill import fill_color
+from .resize import resize, ResizeMethod
 
 def get_padding_size(image: CudaImage, 
-                           padding: int) -> tuple[int, int]:
+                     padding: int) -> tuple[int, int]:
     
     return image.width + (padding * 2), image.height + (padding * 2)
 
@@ -62,3 +63,30 @@ def blend_aligned(background: CudaImage,
     y += offset_y
     
     blend(background, image, x, y)
+
+
+
+def cover_image_in_container(image: CudaImage,
+                             container_width: int,
+                             container_height: int,
+                             offset_x: int = 0,
+                             offset_y: int = 0,
+                             background_color: tuple[int, int, int, int] = (255, 255, 255, 0)
+                             ) -> CudaImage:
+
+    scale = max(container_width / image.width, container_height / image.height)
+    new_width = int(image.width * scale)
+    new_height = int(image.height * scale)
+    
+    resized_image = resize(image, new_width, new_height, method=ResizeMethod.BICUBIC)
+
+    container = CudaImage(container_width, container_height)
+    fill_color(container, background_color)
+    
+    x = (container_width - new_width) // 2 + offset_x
+    y = (container_height - new_height) // 2 + offset_y
+    
+    blend(container, resized_image, x, y)
+    
+    resized_image.free()
+    return container
