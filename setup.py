952dc7fdc01b7b17479b1ba02309d7b8942836a9
offerrall <offerrallps4@gmail.com>
2025-06-19 15:15:21 +0200

    a

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="photoff",
     version="0.0.1",
-    packages=find_packages(),
-    install_requires=[
-        "cffi>=1.17.1",
-    ],
     author="Beltran offerrall",
     author_email="offerrallps4@gmail.com",
     description="A minimal CUDA-based image composition library",
