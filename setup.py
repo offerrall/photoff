c81deceaed6f339cc3698e111c06e46e3c1489e6
offerrall <offerrallps4@gmail.com>
2025-07-06 02:31:58 +0200

    add install_requires in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="photoff",
     version="0.0.1",
-    author="Beltran offerrall",
+    author="Beltran Offerrall",
     author_email="offerrallps4@gmail.com",
     description="A minimal CUDA-based image composition library",
     python_requires=">=3.9",
     packages=find_packages(),
+    install_requires=[
+        "cffi",
+        "Pillow"
+    ],
 )
